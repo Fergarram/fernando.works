b2f81cf9685e3c4e19933eb1b574c80b4e13bd9d
Fernando Garcia <fergarram@gmail.com>
2021-06-02 21:44:55 -0500

    Started working on Podcast page again

--- src/routes/podcast.svelte
+++ src/routes/podcast.svelte
@@ -6,27 +6,27 @@
 	import photo from '../assets/delete-me.png';
 </script>
 
-<section class="bg-yellow pt-24 xs:pt-48 md:pt-56 lg:pt-64 xl:pt-80 pb-40">
-	<div class="max-w-screen-xl mx-auto px-4 xs:px-12 md:px-16 lg:px-20 xl:px-28">
-		<picture class="block mb-16">
+<section class="bg-yellow pt-24 xs:pt-48 md:pt-56 lg:pt-64 pb-12 md:pb-16 lg:pb-20 xl:pb-28">
+	<div class="max-w-screen-xl mx-auto px-6 xs:px-8 sm:px-12 md:px-16 lg:px-20 xl:px-28">
+		<picture class="block mb-8 md:mb-16">
 			<img
 				class="max-w-full h-auto rounded-md"
 				alt="Fernando semi-squating looking at the sky on top of an ancient mexican pyramid."
 				src={photo}
 			/>
 		</picture>
-		<h1 class="font-bold text-44 leading-115 tracking-title sm:text-64 mb-8">
+		<h1 class="font-bold text-36 xs:text-40 sm:text-44 md:text-48 lg:text-56 xl:text-64 leading-115 tracking-title mb-4 md:mb-8">
 			Digital Crafters
 		</h1>
-		<div class="max-w-30em font-light text-20 sm:text-24 leading-150 mb-16">
+		<div class="max-w-30em font-light text-18 xs:text-20 md:text-24 leading-150 mb-8 md:mb-16">
 			<p>
 				I host a podcast called Digital Crafters where I have conversations with independent artists, designers, and developers who make a living out of their digital creations — whether it’s a piece of digital art, a tool, game, or product — they have found a way to fully dedicate themselves to work on the projects they own and love.
 			</p>
 		</div>
-		<ul class="flex flex-wrap mb-10">
+		<ul class="flex flex-wrap">
 			<li class="w-1/2 sm:w-auto flex mb-3 sm:mr-10">
 				<a class="link-button" href="/">
-					Youtube Channel
+					Youtube
 				</a>
 			</li>
 			<li class="w-1/2 sm:w-auto flex mb-3 sm:mr-10">
@@ -36,9 +36,19 @@
 			</li>
 			<li class="w-1/2 sm:w-auto flex mb-3">
 				<a class="link-button" href="/">
-					Amazon Podcasts
+					Amazon
 				</a>
 			</li>
 		</ul>
+	</div>
+</section>
+<section class="bg-yellow pt-12 pb-40">
+	<div class="max-w-screen-xl mx-auto px-6 xs:px-8 sm:px-12 md:px-16 lg:px-20 xl:px-28">
+		<h2 class="font-semibold text-32 sm:text-40 lg:text-44 leading-115 tracking-title mb-6">
+			Episodes
+		</h2>
+		<article>
+			
+		</article>
 	</div>
 </section>

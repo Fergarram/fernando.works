87479c81de1a6808c126266ccf548536a569a8c0
Fernando Garcia <fergarram@gmail.com>
2021-06-09 23:06:04 -0500

    Made about page BE ready.

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,6 +4,57 @@
 
 <script>
 	import photo from '../assets/about.jpg';
+
+	export let fields = {
+		image: {
+			url: photo,
+			alt: 'Fernando semi-squating looking at the sky on top of an ancient mexican pyramid.'
+		},
+		heading: 'About Me',
+		content: `
+			<p>
+				As you could already tell, my name is Fernando Garcia. I am a frontend developer currently based in Mexico. I specialize in web technology although I have <a href="/">created apps</a> for Android and iOS, too.
+			</p>
+			<p>
+				My prefered frontend frameworks are usually VueJS, Svelte, or NextJS. It really depends on who the project is for and what the requirements are.
+			</p>
+			<p>
+				Although, I’ll push for Vue for more complex web applications and Next for headless marketing sites since it’s a more developed ecosystem. For personal projects, I will most likely use Svelte or Vue, though.
+			</p>
+			<h2>
+				More stuff about me lol
+			</h2>
+			<p>
+				I am really passionate about UI design and software development. I love to write about my experiences and document what I learn or observe.
+			</p>
+			<p>
+				Generally, you’ll find me playing around with fun toys like the Raspberry Pi, joining a game jam, or messing around with Ansi C and Linux. I’ve always wanted to create my own OS since I was a child. But, as it turns out, doing so is way more challenging than I would have thought. Still, this doesn’t stop me of dreaming and writing a few notes about <a href="/">how operating systems might evolve in the future</a>.
+			</p>
+			<p>
+				I also happen to host a <a href="/">podcast</a>. I really enjoy talking with other creatives out there making a living (or trying to) by building stuff with their computers. It’s really cool that in this day and age I can simply contact anyone in the world and have a call. It’s just mind blowing.
+			</p>
+			<p>
+				Just how you can send me a message right now ;)
+			</p>
+		`,
+		links: [
+			{
+				label: 'LinkedIn',
+				url: 'https://www.linkedin.com/in/fergarram/',
+				external: true
+			},
+			{
+				label: 'Twitter',
+				url: 'https://twitter.com/_fergarram_/',
+				external: true
+			},
+			{
+				label: 'Email',
+				url: 'mailto:fergarram@gmail.com',
+				external: false
+			},
+		]
+	};
 </script>
 
 <section class="bg-green pb-40">
@@ -11,55 +62,28 @@
 		<picture class="block mb-8 md:mb-16">
 			<img
 				class="w-full max-w-xl h-auto rounded-md"
-				alt="Fernando semi-squating looking at the sky on top of an ancient mexican pyramid."
-				src={photo}
+				alt={ fields.image.alt }
+				src={ fields.image.url }
 			/>
 		</picture>
 		<h1 class="font-semibold text-36 xs:text-40 sm:text-44 md:text-48 lg:text-56 xl:text-64 leading-115 tracking-title mb-4 md:mb-8">
-			About Me
+			{@html fields.heading }
 		</h1>
-		<div class="max-w-30em font-normal text-18 xs:text-20 md:text-24 leading-150 mb-16">
-			<p class="mb-8">
-				As you could already tell, my name is Fernando Garcia. I am a frontend developer currently based in Mexico. I specialize in web technology although I have <a class="underline" href="/">created apps</a> for Android and iOS, too.
-			</p>
-			<p class="mb-8">
-				My prefered frontend frameworks are usually VueJS, Svelte, or NextJS. It really depends on who the project is for and what the requirements are.
-			</p>
-			<p class="mb-8">
-				Although, I’ll push for Vue for more complex web applications and Next for headless marketing sites since it’s a more developed ecosystem. For personal projects, I will most likely use Svelte or Vue, though.
-			</p>
-			<h2 class="font-semibold text-32 sm:text-40 lg:text-44 leading-115 tracking-title mt-12 mb-6">
-				More stuff about me lol
-			</h2>
-			<p class="mb-8">
-				I am really passionate about UI design and software development. I love to write about my experiences and document what I learn or observe.
-			</p>
-			<p class="mb-8">
-				Generally, you’ll find me playing around with fun toys like the Raspberry Pi, joining a game jam, or messing around with Ansi C and Linux. I’ve always wanted to create my own OS since I was a child. But, as it turns out, doing so is way more challenging than I would have thought. Still, this doesn’t stop me of dreaming and writing a few notes about <a class="underline" href="/">how operating systems might evolve in the future</a>.
-			</p>
-			<p class="mb-8">
-				I also happen to host a <a class="underline" href="/">podcast</a>. I really enjoy talking with other creatives out there making a living (or trying to) by building stuff with their computers. It’s really cool that in this day and age I can simply contact anyone in the world and have a call. It’s just mind blowing.
-			</p>
-			<p>
-				Just how you can send me a message right now ;)
-			</p>
+		<div class="page-content max-w-30em font-normal text-18 xs:text-20 md:text-24 leading-150 mb-16">
+			{@html fields.content }
 		</div>
-		<ul class="flex flex-wrap mb-10">
-			<li class="w-1/2 sm:w-auto flex mb-3 sm:mr-10">
-				<a class="link-button" href="/">
-					LinkedIn
-				</a>
-			</li>
-			<li class="w-1/2 sm:w-auto flex mb-3 sm:mr-10">
-				<a class="link-button" href="/">
-					Twitter
-				</a>
-			</li>
-			<li class="w-1/2 sm:w-auto flex mb-3">
-				<a class="link-button" href="/">
-					Email
-				</a>
-			</li>
+		<ul class="flex mb-10">
+			{#each fields.links as link}
+				<li class="w-1/3 sm:w-auto flex mb-3 sm:mr-10 last:mr-0">
+					<a
+						class="link-button"
+						href={ link.url }
+						target={ link.external ? '_blank' : null }
+						rel={ link.external ? 'noopener' : 'prefetch' }>
+						{ link.label }
+					</a>
+				</li>
+			{/each}
 		</ul>
 	</div>
 </section>
